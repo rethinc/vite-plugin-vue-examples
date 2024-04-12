d2c08598b2681d8db8c05f0410d2b619a6b06538
Chris Weber <chris.weber@rethinc.ch>
2024-04-12 13:01:48 +0200

    Add entry point for vite vue examples plugin

---
+++ src/vite-plugin-vue-examples.ts
@@ -0,0 +1,7 @@
+import { PluginOption } from 'vite'
+
+export default (): PluginOption => {
+  return {
+    name: 'vue-examples',
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueExamples from './src/vite-plugin-vue-examples'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueExamples()],
 })
