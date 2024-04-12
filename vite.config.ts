582663d0ec9d0e5961913c91560dbdbf0b98138e
Chris Weber <chris.weber@rethinc.ch>
2024-04-12 13:05:25 +0200

    Add build configuration for vite/rollup

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import vueExamples from './src/vite-plugin-vue-examples'
 
 export default defineConfig({
   plugins: [vue(), vueExamples()],
+  build: {
+    lib: {
+      entry: 'src/vite-plugin-vue-examples.ts',
+      fileName: 'vite-plugin-vue-examples',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['vite'],
+    },
+  },
 })
