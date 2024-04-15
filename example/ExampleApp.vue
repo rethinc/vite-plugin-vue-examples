5a8f361e12b93655fd707de243c462b2e54c67b1
Chris Weber <chris.weber@rethinc.ch>
2024-04-15 15:13:08 +0200

    Remove test implementation to output generated routes

--- example/ExampleApp.vue
+++ example/ExampleApp.vue
@@ -1,7 +1,3 @@
-<script lang="ts" setup>
-import { routeRecords } from 'virtual:vue-examples-route-records'
-</script>
 <template>
   <h1>Example App for vite-plugin-vue-examples</h1>
-  <p>{{ JSON.stringify(routeRecords) }}</p>
 </template>
