a25fdcc9be047d5642de1e2e6535f59bb72455c0
Chris Weber <chris.weber@rethinc.ch>
2024-04-15 12:51:19 +0200

    Add showing examples app at specific path in dev mode

--- src/vite-plugin-vue-examples.ts
+++ src/vite-plugin-vue-examples.ts
@@ -1,25 +1,32 @@
 import * as path from 'path'
-import { PluginOption } from 'vite'
+import * as fsp from 'fs/promises'
+import { PluginOption, send } from 'vite'
 import { mapExamplesToRoutes } from './map-examples-to-routes'
 import { generateRouteRecordsJavascript } from './generate-route-records-javascript'
 
 export interface VueExamplesPluginConfiguration {
   examplesRootPath: string
   exampleFileNameSuffix: string
+  examplesAppPath: string
 }
 
 export default (
   customConfiguration: Partial<VueExamplesPluginConfiguration> = {}
 ): PluginOption => {
-  const { exampleFileNameSuffix = '.example.vue', examplesRootPath = './src' } =
-    customConfiguration
+  const {
+    exampleFileNameSuffix = '.example.vue',
+    examplesRootPath = './src',
+    examplesAppPath = 'vue-examples',
+  } = customConfiguration
   const configuration: VueExamplesPluginConfiguration = {
     examplesRootPath,
     exampleFileNameSuffix,
+    examplesAppPath: path.resolve('/', examplesAppPath) + '/',
   }
 
   const routeRecordsId = 'virtual:vue-examples-route-records'
   const resolvedRouteRecordsId = '\0' + routeRecordsId
+  const examplesAppFolder = path.resolve(__dirname, 'examples-app/')
 
   return {
     name: 'vue-examples',
@@ -29,9 +36,29 @@ export default (
         configuration.examplesRootPath
       )
     },
-    resolveId(id) {
+    configureServer: (server) => {
+      server.middlewares.use(async (req, res, next) => {
+        if (req.url === configuration.examplesAppPath) {
+          const htmlFilePath = path.join(examplesAppFolder, 'index.html')
+          const htmlUrl = path.join(req.url, 'index.html')
+          const originalHtml = await fsp.readFile(htmlFilePath, 'utf-8')
+          const html = await server.transformIndexHtml(htmlUrl, originalHtml)
+          send(req, res, html, 'html', {})
+          return
+        }
+        next()
+      })
+    },
+
+    async resolveId(id) {
       if (id === routeRecordsId) {
         return resolvedRouteRecordsId
+      }
+      if (id.startsWith(configuration.examplesAppPath)) {
+        const relativeFilePath = id.replace(configuration.examplesAppPath, '')
+        const absoluteFilePath = path.join(examplesAppFolder, relativeFilePath)
+        const resolvedPath = await this.resolve(absoluteFilePath)
+        return resolvedPath?.id ?? id
       }
     },
     async load(id) {
