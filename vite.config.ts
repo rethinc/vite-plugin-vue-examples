45bc1feecbd8a5213557667df8120f2b69a00a7a
Chris Weber <chris.weber@rethinc.ch>
2024-04-17 11:01:52 +0200

    Add generating type definitions when building plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 import vueExamples from './src/vite-plugin-vue-examples'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueExamples({ globalStylesheets: ['/example/global.scss'] }),
+    dts({ include: ['src/vite-plugin-vue-examples.ts'] }),
   ],
   build: {
     lib: {
