81b23e7e1dddcbed96cc732099928221329e0128
Chris Weber <chris.weber@rethinc.ch>
2024-04-29 12:02:58 +0200

    Update eslint config and packages

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import eslintJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintPluginVue from 'eslint-plugin-vue'
+import typescriptEslint from 'typescript-eslint'
+
+export default typescriptEslint.config(
+  eslintJs.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  ...eslintPluginVue.configs['flat/recommended'],
+
+  {
+    plugins: {
+      'typescript-eslint': typescriptEslint.plugin,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+    languageOptions: {
+      parserOptions: {
+        parser: typescriptEslint.parser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    ignores: ['dist/*'],
+  },
+  eslintPluginPrettierRecommended
+)
