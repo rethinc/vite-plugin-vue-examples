520524405658e63e2bc4272e5b4ee4af43ce8db1
Chris Weber <chris.weber@rethinc.ch>
2024-05-03 21:49:21 +0200

    Show examples starting from 'src' directory in dev-app

--- dev-app/vite.config.ts
+++ dev-app/vite.config.ts
@@ -5,6 +5,9 @@ import vueExamples from 'vite-plugin-vue-examples'
 export default defineConfig({
   plugins: [
     vue(),
-    vueExamples({ globalStylesheetPaths: ['/src/global.scss'] }),
+    vueExamples({
+      globalStylesheetPaths: ['/src/global.scss'],
+      examplesRootPath: 'src',
+    }),
   ],
 })
